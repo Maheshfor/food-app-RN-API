cd54d7bc0e18da931de7422f5482bd91808b6bf6
Maheshfor <56917544+Maheshfor@users.noreply.github.com>
2022-03-08 20:17:39 +0800

    this is food app in RN API project

---
+++ App.js
@@ -0,0 +1,35 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import Apicall from './apicall';
+import BasicStyleComponents from './basicStyleComponents';
+import DetailPage from './detailPage';
+import RNForm from './rnForm';
+//import ListData from './listData';
+import Paytmhome from './paytmhome';
+//import Webview from './webview';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Paytmhome"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Apicall" component={Apicall} />
+        <Stack.Screen name="DetailPage" component={DetailPage} />
+        <Stack.Screen name="RNForm" component={RNForm} />
+        <Stack.Screen
+          name="BasicStyleComponents"
+          component={BasicStyleComponents}
+        />
+        <Stack.Screen name="Paytmhome" component={Paytmhome}></Stack.Screen>
+        <Stack.Screen name="Webview" component={Webview}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
